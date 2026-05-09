316caded3d3bd6563f086784dcd4a3ac4a6b0c1c
MohamedYasser3458 <m.elhaddad2006@gmail.com>
2026-05-09 13:49:17 +0300

    Favorite

---
+++ app/controllers/FavoriteController.php
@@ -0,0 +1,16 @@
+<?php 
+
+class FavoriteController extends BaseController {
+
+    public function index(){
+
+    }
+
+    public function add(){
+
+    }
+
+    public function remove(){
+
+    }
+}
